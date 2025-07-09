9689655a37c8193314fa6b30d6abf0b6d31c3a9c
root <root@localhost.localdomain>
2025-07-09 11:49:15 +0800

    add graphrad

---
+++ app/backend/agent.py
@@ -0,0 +1,59 @@
+from langchain_mcp_adapters.client import MultiServerMCPClient
+from langgraph.prebuilt import create_react_agent
+from langchain_openai import ChatOpenAI
+import asyncio
+from flask import Flask, request, jsonify
+from langchain_core.messages import HumanMessage, AIMessage
+from langgraph.checkpoint.memory import MemorySaver
+
+app = Flask(__name__)
+
+
+llm = ChatOpenAI(
+    model = 'deepseek-chat',
+    temperature=0.8,
+    base_url = 'https://api.deepseek.com',
+    api_key = 'sk-5c96f4b3f885469eb9af52bab183d654',
+)
+
+
+client = MultiServerMCPClient({
+    "medicine": {
+        "url": "http://localhost:8000/mcp",
+        "transport": "streamable_http"
+    }
+})
+
+agent = None
+checkpointer = MemorySaver()
+async def init_agent():
+    global agent
+    tools = await client.get_tools()
+    agent = create_react_agent(llm, tools, checkpointer=checkpointer)
+
+asyncio.run(init_agent())
+#tools = asyncio.run(client.get_tools())
+#agent = create_react_agent(llm, tools, checkpointer=checkpointer)
+
+@app.route("/chat", methods=["POST"])
+def chat():
+    data = request.json
+    msg = data.get("message")
+    sid = data.get("session_id", "default")
+    if not msg:
+        return jsonify({"error": "message 必填"}), 400
+
+    async def run():
+        resp = await agent.ainvoke(
+            {"messages": [HumanMessage(content=msg)]},
+            config={"configurable":{"thread_id": sid}}
+        )
+        # resp 是 BaseMessage 类型，使用 .content 获取文本
+        #return resp.content if hasattr(resp, "content") else str(resp)
+        return resp["messages"][-1].content
+
+    reply = asyncio.run(run())
+    return jsonify({"reply": reply})
+
+if __name__ == "__main__":
+   app.run(host="0.0.0.0", port=5000, debug=True)
---
+++ app/llm/mcp_server/graphrag/rag_server.py
@@ -0,0 +1,55 @@
+from pathlib import Path
+from pprint import pprint
+
+import pandas as pd
+
+import graphrag.api as api
+from graphrag.config.load_config import load_config
+from graphrag.index.typing.pipeline_run_result import PipelineRunResult
+
+from typing import Any
+from fastmcp import FastMCP
+
+# 初始化MCP服务器
+mcp = FastMCP("rag_ML")
+USER_AGENT = "rag_ML-app/1.0"
+
+@mcp.tool()
+async def rag_ML(query: str) -> str:
+    """
+    用于查询疾病诊疗和处方审核相关信息。
+    :param query: 用户提出的具体问题
+    :return: 最终获得的答案
+    """
+    PROJECT_DIRECTORY = "/root/mcp-graphrag/graphrag"
+    graphrag_config = load_config(Path(PROJECT_DIRECTORY))
+
+    # 加载实体
+    entities = pd.read_parquet(f"{PROJECT_DIRECTORY}/output/entities.parquet")
+    # 加载社区
+    communities = pd.read_parquet(f"{PROJECT_DIRECTORY}/output/communities.parquet")
+    # 加载社区报告
+    community_reports = pd.read_parquet(f"{PROJECT_DIRECTORY}/output/community_reports.parquet")
+
+    # 进行全局搜索
+    response, context = await api.global_search(
+        config=graphrag_config,
+        entities=entities,
+        communities=communities,
+        community_reports=community_reports,
+        community_level=2,
+        dynamic_community_selection=False,
+        response_type="Multiple Paragraphs",
+        query=query,
+    )
+
+    return response
+
+if __name__ == "__main__":
+    mcp.run(
+        transport='http',
+        host="0.0.0.0",
+        port=8000,
+        path="/mcp",
+        log_level="info"
+    )
